52bc7206f0b370f9eb260d4b6174aea771556bfd
PerryZ <69716263+Perry2004@users.noreply.github.com>
2022-06-08 19:37:00 +0800

    add fetch

---
+++ js-websites/fetch/fetch.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  const p = document.querySelector("p");
+  const btn1 = document.querySelector("#btn-1");
+  const btn2 = document.querySelector("#btn-2");
+
+  btn1.onclick = () => {
+    fetch("。/txt-1.txt").then((text) => {
+      p.innerHTML = text;
+    }).catch((error) => {
+      p.innerHTML = error;
+    })
+  }
+
+  btn2.onclick = () => {
+    fetch("。/txt-2.txt").then((text) => {
+      p.innerHTML = text;
+    }).catch((error) => {
+      p.innerHTML = error;
+    })
+  }
+}
