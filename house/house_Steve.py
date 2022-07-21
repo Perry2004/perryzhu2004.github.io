02c9805bff8fc88a10c2cceeb7e47eb366f60b17
PerryZ <69716263+Perry2004@users.noreply.github.com>
2022-07-21 21:13:03 +0800

    house-drawing homework

---
+++ house/house_Steve.py
@@ -0,0 +1,25 @@
+import turtle
+turtle.color("green")
+count=0
+while count< 4 :
+    turtle.fd(80)
+    turtle.lt(90)
+    count=count+1
+turtle.fd(20)
+turtle.lt(90)
+count=0
+while count< 3 :
+    turtle.fd(40)
+    turtle.rt(90)
+    count=count+1
+turtle.bk(20)
+turtle.rt(90)
+turtle.fd(80)
+turtle.color("red")
+turtle.lt(30)
+turtle.fd(80)
+turtle.lt(120)
+turtle.fd(80)
+turtle.lt(120)
+turtle.fd(80)
+
---
+++ house/house_andy.py
@@ -0,0 +1,23 @@
+Python 3.10.5 (v3.10.5:f377153967, Jun  6 2022, 12:36:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle as t
+t.forward(20)
+t.left(90)
+t.forward(40)
+t.left(270)
+t.forward(40)
+t.left(270)
+t.forward(40)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(80)
+t.left(30)
+t.forward(80)
+t.left(60)
+t.left(60)
+t.forward(80)
+t.left(30)
+t.forward(80)
+t.left(90)
+t.forward(80)
